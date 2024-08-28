22fd18502eab19e4a0b4eac7878720bca2e013c6
Denis Lienskyi <whitcr0k@gmail.com>
2024-08-28 15:50:10 +0200

    fixed

--- helpers/validateBody.js
+++ helpers/validateBody.js
@@ -3,9 +3,19 @@ import HttpError from "./HttpError.js";
 const validateBody = (schema) => {
   const func = (req, _, next) => {
     const { error } = schema.validate(req.body);
+
     if (error) {
-      next(HttpError(400, "Помилка від Joi або іншої бібліотеки валідації"));
+      const errorMessage = error.details
+        .map((detail) => detail.message)
+        .join(", ");
+      return next(
+        HttpError(
+          400,
+          `Помилка валідації: ${errorMessage}. Перевірте правильність даних.`
+        )
+      );
     }
+
     next();
   };
 
